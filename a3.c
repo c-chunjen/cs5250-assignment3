5044e367a89c23e9b2d776360b863bb0af30cc4f
chunren78 <chunjen.chiu@outlook.com>
2017-03-22 17:53:22 +0800

    Update a3.c

--- a3.c
+++ a3.c
@@ -39,6 +39,16 @@ int onebyte_release(struct inode *inode, struct file *filep)
 ssize_t onebyte_read(struct file *filep, char *buf, size_t count, loff_t *f_pos)
 {
 	/*please complete the function on your own*/
+	
+	int bytes_read = 0;
+	
+	while(count && *onebyte_data) {
+		put_user(*(onebyte_data++), buf++);
+		bytes_read++;
+		count--;
+	}
+	
+	return bytes_read;
 }
 
 ssize_t onebyte_write(struct file *filep, const char *buf, size_t count, loff_t *f_pos)
